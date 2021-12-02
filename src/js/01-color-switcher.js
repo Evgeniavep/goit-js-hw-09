5ac95c0cf5f9614a4e6cd8dbe43fd352b4bced49
Evhenia Vepretskaya <enychka12@gmail.com>
2021-12-02 13:35:52 +0200

    color-switcher done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timerInterval = null;
 
+startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick);
+
+function onStartClick() {
+    if (!startBtn.hasAttribute('inactive')) {
+        startBtn.setAttribute('inactive', 'inactive')
+        changeBodyColor();
+    }
+};
+
+
+function onStopClick() {
+    startBtn.removeAttribute('inactive', 'inactive');
+    clearInterval(timerInterval);
+};
+
+
+function changeBodyColor() {
+    timerInterval = setInterval(() => {
+        const newColor = getRandomHexColor();
+        body.style.backgroundColor = newColor;
+    }, 1000);
+};
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
