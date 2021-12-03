c05cdc6bde50fbb91120d5c501a05d71d125d38b
Evhenia Vepretskaya <enychka12@gmail.com>
2021-12-03 14:06:33 +0200

    timer done

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -9,6 +9,10 @@ const hoursLeft = document.querySelector('[data-hours]');
 const minutesLeft = document.querySelector('[data-minutes]');
 const secondsLeft = document.querySelector('[data-seconds]');
 
+let interval = null;
+let userDate = null;
+let timeLeft = null;
+
 
 const options = {
     enableTime: true,
@@ -16,14 +20,62 @@ const options = {
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-        if (selectedDates[0] <= this.defaultDate) {
-            window.alert("Please choose a date in the future");
+        if (selectedDates[0] <= options.defaultDate) {
+            alert("Please choose a date in the future");
             startBtn.setAttribute('disabled', 'disabled');
-        };
-        return;
+            return;
+        }
+        startBtn.removeAttribute('disabled', 'disabled');
+        userDate = selectedDates[0];
+        daysLeft.textContent = '00';
+        hoursLeft.textContent = '00';
+        minutesLeft.textContent = '00';
+        secondsLeft.textContent = '00';
     },
+
 };
 
 flatpickr('#datetime-picker', options);
 
-function onStartClick() {};
+
+function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+    return { days, hours, minutes, seconds };
+};
+
+
+const timer = () => {
+    timeLeft = userDate - new Date();
+    let timerData = convertMs(timeLeft);
+    daysLeft.textContent = addLeadingZero(timerData.days);
+    hoursLeft.textContent = addLeadingZero(timerData.hours);
+    minutesLeft.textContent = addLeadingZero(timerData.minutes);
+    secondsLeft.textContent = addLeadingZero(timerData.seconds);
+    if (timeLeft <= 500) {
+        clearInterval(interval);
+    }
+};
+
+
+function onStartClick() {
+    startBtn.setAttribute('disabled', 'disabled');
+    interval = setInterval(timer, 1000);
+};
+
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+};
